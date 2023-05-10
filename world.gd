b4c9f9a2ab52a5f80d61091d2d2224f1900646ee
Sidney de Vries <sidney.de.vries96@gmail.com>
2023-05-11 00:13:20 +0200

    Prevent trees from spawning in river or directly next to eachother in world generation

--- world.gd
+++ world.gd
@@ -6,17 +6,19 @@ const OFFSET = TILESIZE / 2
 @onready var riverTileMap = $RiverTileMap
 @onready var background = $Background.get_rect()
 
+# Width and height in tiles, not pixels
 @onready var mapHeight = floor(background.size.y / TILESIZE)
 @onready var mapWidth = floor(background.size.x / TILESIZE)
+
+var usedTiles = []
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(mapWidth, "|", mapHeight)
-	generateTrees(50)
 	generateRiver()
+	generateTrees(50)
 	
-
 
 func generateRiver():
 	const MIN_DOWN_STEPS = 5
@@ -28,11 +30,12 @@ func generateRiver():
 	var riverCells = []
 	# For now always start river in middle
 	var currentCell = Vector2(floor(mapWidth / 2), -1)
-	
 	riverCells.append(currentCell)
 	
+	# Keep track of amount of iterations, use this in deciding
+	# wether to go down or sideways
 	var stepCount = 0
-	
+	# Used to stop while loop when river reached bottom of map
 	var bottomReached = false
 	
 	while not bottomReached:
@@ -52,7 +55,7 @@ func generateRiver():
 			updateVector = Vector2.DOWN
 			
 		for i in range(stepsToTake):
-			if (currentCell + updateVector).y > mapHeight:
+			if (currentCell + updateVector).y >= mapHeight:
 				bottomReached = true
 			elif (
 				(currentCell + updateVector).x < mapWidth - 1 - WIDTH and
@@ -70,28 +73,54 @@ func generateRiver():
 		stepCount += 1
 		
 	riverTileMap.set_cells_terrain_connect(0, riverCells, 0, 0)
+	usedTiles.append_array(riverCells)
 
 
 func generateTrees(n: int = 10):
-	var treesNode = get_tree().get_root().get_node("World/ysort/Trees")#.current_scene
-
+	var treesNode = get_tree().get_root().get_node("World/ysort/Trees")
 	var TreeFactory = load("res://World/Tree.tscn")
 	
-	for i in range(n):
-		var tree = TreeFactory.instantiate()
-		treesNode.add_child(tree)
-		tree.global_position = randomCoordinateVector()
-
+	var succesfulPlacements = 0
+	while succesfulPlacements < n:
+		var newTreeCoordinates = randomCoordinateVector()
+		var newTreeTileCoordinates = coordinateToTile(newTreeCoordinates)
+		
+		# We dont want to place a tree if there is a river tile or other
+		# tree in the tiles around. Use computationally expensive nested loop
+		# to make sure the tiles around the potential new position are free
+		var borderTilesFree = []
+		for x in range(newTreeTileCoordinates.x - 1, newTreeTileCoordinates.x + 2):
+			for y in range(newTreeTileCoordinates.y - 1, newTreeTileCoordinates.y + 2):
+				borderTilesFree.append(Vector2(x, y) not in usedTiles)
+		
+		if not false in borderTilesFree:
+			var tree = TreeFactory.instantiate()
+			treesNode.add_child(tree)
+			tree.global_position = newTreeCoordinates
+			
+			succesfulPlacements += 1
+			usedTiles.append(newTreeTileCoordinates)
 
 func spawnPlayer():
 	pass
 
-func randomCoordinateVector():
+func randomCoordinateVector() -> Vector2:
 	# Exclude border tiles
-	var tileCoordinates = Vector2i(
+	var tileCoordinates = Vector2(
 		randi_range(2, mapWidth - 2),
 		randi_range(2, mapHeight - 2)
 	)
-	# Transform tile coordinates to pixel coordinates
-	# Offset is used to go in middle of tile instead of corner
-	return (tileCoordinates * TILESIZE) - Vector2i(OFFSET, OFFSET)
+	# Transform tile coordinates to pixel coordinates of center of tile
+	return tileToCoordinate(tileCoordinates)
+
+func tileToCoordinate(tile: Vector2) -> Vector2:
+	# Convert tile coordinates to pixel coordinates
+	# Pixel coordinates are center of the tile
+	# Vector2(0, 0) -> Vector2(8, 8)
+	return ((tile + Vector2.ONE) * TILESIZE) - Vector2(OFFSET, OFFSET)
+	
+func coordinateToTile(coordinates: Vector2) -> Vector2:
+	# Convert pixel coordinates to tile coordinates
+	# Vector(7, 1) -> Vector(0, 0)
+	return floor(coordinates / float(TILESIZE))
+
